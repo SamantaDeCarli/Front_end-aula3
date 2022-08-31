e1758a0a58293f0d0be23c0df0bf46603a633cc6
Samanta <110949190+SamantaDeCarli@users.noreply.github.com>
2022-08-30 22:13:47 -0300

    Add files via upload

---
+++ scricpt3_02.js
@@ -0,0 +1,9 @@
+let aluno, nota;
+aluno = "Astrogildo";
+nota = "9.5";
+cep = "03350123"
+document.write ("aluno: ",aluno);
+document.write ("<br>");
+document.write ("nota: ", nota);
+document.write("<br>");
+document.write ("cep:", cep)
